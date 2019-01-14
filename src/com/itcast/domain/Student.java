739a9de9528e8798d7d1db879c92d9c5de24e582
cb199177 <974355655@qq.com>
2019-01-14 18:05:52 +0800

    第一次提交

---
+++ src/com/itcast/domain/Student.java
@@ -0,0 +1,28 @@
+package com.itcast.domain;
+
+public class Student {
+	
+	private int age;
+	private String name;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [age=" + age + ", name=" + name + "]";
+	}
+	
+	
+	
+
+}
